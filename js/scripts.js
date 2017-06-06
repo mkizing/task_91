c16cdf8ba7b645f69f0d91b939ef8b5fe1c9f86e
Michal Kizling <mkizling@gmail.com>
2017-06-06 22:41:58 +0200

    var addition in js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function getTriangleArea(a,h) {
 		console.log('You selected improper value');
 	}
 	else {
-		return a * h / 3;
+		return a * h / 2;
 	}
 }
+
+var triangle1Area = getTriangleArea(5,6);
+var triangle2Area = getTriangleArea(10,7);
+var triangle3Area = getTriangleArea(3,8);
